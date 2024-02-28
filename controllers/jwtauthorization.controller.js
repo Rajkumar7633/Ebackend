ac4b56df3e82e16e8b7e636253dd6d1b4324fcac
Ritik Raj <80347492+raj4477@users.noreply.github.com>
2024-02-28 13:40:57 +0530

    ch

--- controllers/jwtauthorization.controller.js
+++ controllers/jwtauthorization.controller.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 const jwtAuthorization = async (req, res, next) => {
     const authorizationHeader = req.header("Authorization")
@@ -12,12 +13,13 @@ const jwtAuthorization = async (req, res, next) => {
         return res.status(401).json({ error: "No Token was present", message: "Authorization token not found" });
     }
     try {
-        const decoded = jwt.verify(token, "esuchana123esuchan123secret123");
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
         console.log(decoded)
         req.user_email = decoded.email;
         req.user_level = decoded.level;
         next();
       } catch (err) {
+        console.log(err);
         return res.status(401).json({ error: "error", message: "Invalid token" });
       }
 
